09006e8308ee617b89a1ba4f5a33dab6c240bc13
Quentin Troemner <31808836+qdtroemner@users.noreply.github.com>
2020-10-18 20:50:58 -0400

    Update script.js
    Update for v2

--- js/script.js
+++ js/script.js
@@ -1,17 +1,1 @@
-window.onload = () => {
-	var next = document.getElementById("to-about"),
-		 prev = document.getElementById("to-home");
-
-	var home = document.getElementById("home"),
-		 about = document.getElementById("about");
-	
-	next.addEventListener("click", () => {
-		home.style.display = "none";
-		about.style.display = "flex";
-	}, true);
-	
-	prev.addEventListener("click", () => {
-		about.style.display = "none";
-		home.style.display = "flex";
-	});
-}
+// No JS needed for v2 yet...
