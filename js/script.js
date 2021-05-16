2d3e41dae9e11f25562cea649ea7b3f4d78d931a
qdtroemner <qdtroemner@outlook.com>
2021-05-16 00:30:12 -0400

    links

--- js/script.js
+++ js/script.js
@@ -9,10 +9,38 @@ const images = [
 
 window.addEventListener('load', function() {
     var i = 0;
+    //document.getElementById("time").innerHTML = getTime();
+    document.body.style.backgroundImage = `url(${images[i]})`;
+
 	window.setInterval(function() {
-        console.log(i);
-        document.body.style.backgroundImage = `url(${images[i]})`;
         i++;
         if (i >= images.length) {i = 0;}
+        document.body.style.backgroundImage = `url(${images[i]})`;
     }, 2000);
+
+    /*window.setInterval(function() {
+        t = new Date(), h = t.getHours(), m = t.getMinutes();
+        if (h > 12) {
+            h = h - 12;
+        }
+        if (h == 0) {
+            h = 12;
+        }
+        document.getElementById("time").innerHTML = getTime();
+    }, 60000);*/
 })
+
+function getTime() {
+    t = new Date(), h = t.getHours(), m = t.getMinutes(), p = "AM";
+    if (h > 12) {
+        h = h - 12;
+        p = " PM";
+    }
+    if (h == 0) {
+        h = 12;
+    }
+    if (m < 10) {
+        m = "0" + m;
+    }
+    return `${h}:${m} ${p}`;
+}
